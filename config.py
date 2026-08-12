056de458daf8cdd89fb9d42a9cec906d334c4531
Richophie <reallay7777@gmail.com>
2026-08-13 03:48:36 +0900

    config: remove practical aggregate cap and bump app version

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-APP_VERSION = "16.1"
+APP_VERSION = "16.2"
 CORE_VERSION = "4.2"
 S_THRESHOLD = 85.0
-ELITE_MAX = 5
+ELITE_MAX = 9999
 PUBLIC_STRATEGIES = (
     "confirmed_pullback",
     "rsi2_trend_reversion",
