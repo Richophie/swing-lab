10744b41f1886a3c6c166ae23a3860e5ab7b153d
Richophie <reallay7777@gmail.com>
2026-08-13 02:51:23 +0900

    refactor: add single source of truth for app configuration

---
+++ config.py
@@ -0,0 +1,10 @@
+APP_VERSION = "16.0"
+CORE_VERSION = "4.1"
+S_THRESHOLD = 85.0
+PUBLIC_STRATEGIES = (
+    "confirmed_pullback",
+    "rsi2_trend_reversion",
+    "momentum_pullback",
+)
+EXPERIMENTAL_STRATEGIES = ("volatility_breakout",)
+SCAN_CANDIDATE_LIMIT = 700
