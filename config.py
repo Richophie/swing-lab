9e1c5291df6270c0c660510a5956e3933fe0304c
Richophie <reallay7777@gmail.com>
2026-08-13 04:18:02 +0900

    feat: tighten main universe to established liquid stocks

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-APP_VERSION = "16.4"
-CORE_VERSION = "4.3"
+APP_VERSION = "16.5"
+CORE_VERSION = "4.4"
 S_THRESHOLD = 85.0
 ELITE_MAX = 9999
 PUBLIC_STRATEGIES = (
@@ -8,4 +8,8 @@ PUBLIC_STRATEGIES = (
     "momentum_pullback",
 )
 EXPERIMENTAL_STRATEGIES = ("volatility_breakout",)
-SCAN_CANDIDATE_LIMIT = 700
+# Main scan intentionally avoids the full US micro/small-cap universe.
+SCAN_CANDIDATE_LIMIT = 500
+MAIN_MIN_MARKET_CAP = 2_000_000_000
+MAIN_MIN_AVG_DAILY_VOLUME = 500_000
+MAIN_MIN_PRICE = 5
