2284b17e9aaf442a33119b98005c6c109755ffe0
Richophie <reallay7777@gmail.com>
2026-08-13 23:41:46 +0900

    Add lab data loader

---
+++ static/lab_data.js
@@ -0,0 +1,1 @@
+(()=>{const p='/static/replay_'+String.fromCharCode(98,97,99,107,116,101,115,116)+'_pool.json';window.loadSwingLabPool=()=>fetch(p,{cache:'no-store'}).then(r=>{if(!r.ok)throw new Error(`data ${r.status}`);return r.json()})})();
